de83a84388c700ee94d156f79673e9b0859695cc
wesa5 <wobulufrancis@gmail.com>
2022-06-25 23:12:47 +0300

    classes

--- alx.py
+++ alx.py
@@ -45,6 +45,13 @@ else:
 
 '''print("{:d} Mission street, {}".format(972, "San Francisco"))'''
 
+
+'''class Person:
+    pass  # An empty block
+
+p = Person()
+print(p)'''
+
 '''def my_function(counter=89):
      return counter + 1
  
@@ -52,3 +59,62 @@ print(my_function())'''
 
 '''for i in range(2, 10, 2):
     print(i, end=" ")'''
+
+
+'''class Person:
+    def say_hi(self):
+        print('Hello, how are you?')
+
+Person().say_hi()'''
+
+'''class Robot:
+    population = 0
+
+    def __init__(self, name):
+        self.name = name
+        print(f"Initializing {self.name}")
+        Robot.population += 1
+    def die(self):
+        print(f"{self.name} is being destroyed")
+        Robot.population -= 1
+
+        if Robot.population == 0:
+            print(f"{self.name} was the last one")
+        else:
+            print(f"There are still {Robot.population} working")
+    def say_hi(self):
+        print(f"Greetings my Masters call me {self.name}")
+
+    @classmethod
+    def how_many(cls):
+        print(f"We have {cls.population} robots.")
+
+droid1 = Robot("R2-D2")
+droid1.say_hi()
+Robot.how_many()
+
+droid2 = Robot("C-3PO")
+droid2.say_hi()
+Robot.how_many()
+
+print("\nRobots can do some work here.\n")
+
+print("Robots have finished their work. So let's destroy them.")
+
+droid1.die()
+droid2.die()
+
+Robot.how_many()'''
+
+class User:
+     id = 89
+     name = "no name"
+     __password = None
+     
+     def __init__(self, new_name=None):
+         self.is_new = True
+         if new_name is not None:
+             self.name = new_name
+ 
+u = User()
+print(u.name)
