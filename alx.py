0c39ac5873e8625c95b36219911dd76c83fd4f9a
wesa5 <wobulufrancis@gmail.com>
2022-06-25 19:03:41 +0300

    more practice

--- alx.py
+++ alx.py
@@ -19,3 +19,36 @@ print(my_function())'''
 
 #__import__("sys").stdout.write("#pythoniscool\n")
 
+'''def my_function(counter=89):
+    print("Counter: {}".format(counter))
+
+my_function(12)'''
+
+'''a = "Python is cool"
+print(a[7:-5])'''
+
+'''a = { 'id': 89, 'name': "John", 'projects': [1, 2, 3, 4], 'friends': [ { 'id': 82, 'name': "Bob" }, { 'id': 83, 'name': "Amy" } ] }
+print(a.get('friends')[-1].get("name"))'''
+
+'''a = [1, 2, 3, 4]
+a[2] = 10
+print(a)'''
+
+'''a = 12
+if a > 2:
+    if a % 2 == 0:
+        print("Tech")
+    else:
+        print("C is fun")
+else:
+    print("School")'''
+
+'''print("{:d} Mission street, {}".format(972, "San Francisco"))'''
+
+'''def my_function(counter=89):
+     return counter + 1
+ 
+print(my_function())'''
+
+'''for i in range(2, 10, 2):
+    print(i, end=" ")'''
